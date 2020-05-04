bd003cc648d48a6526bdc1f8b40689a0f5542c4c
supardi9512 <supardi9512@gmail.com>
2020-05-05 00:32:56 +0700

    component bootstrap media object

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="profile">
-    <h1>This is an profile page</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="media">
+                    <img class="mr-3">
+                    <div class="media-body">
+                        <h5 class="mt-0">Media heading</h5>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+
 export default {
   name: 'Profile',
   components: {
