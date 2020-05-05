27598157ed82c76b1fcc03dd873aef54bc55745d
supardi9512 <supardi9512@gmail.com>
2020-05-06 00:31:12 +0700

    membuat data json

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,7 +6,7 @@
                 <div class="media text-left">
                     <img src="../assets/foto.jpg" class="user-pic mr-3">
                     <div class="media-body">
-                        <h5 class="mt-0">Supardi</h5>
+                        <h5 class="mt-0">{{ firstName }} {{ lastName }}</h5>
                         <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</p>
                     </div>
                 </div>
@@ -21,6 +21,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 export default {
   name: 'Profile',
+  data: function() {
+    return {
+      firstName: 'Fardhi',
+      lastName: 'Fathonah'
+    }
+  },
   components: {
 
   }
